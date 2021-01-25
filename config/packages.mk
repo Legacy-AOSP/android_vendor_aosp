8ea666c330f96051fdbbc9118d5396eca8ff6bab
LuK1337 <priv.luk@gmail.com>
2021-01-25 11:24:49 +0100

    vendor: Introduce {Lato,Rubik} font overlay packages

--- config/packages.mk
+++ config/packages.mk
@@ -1,6 +1,11 @@
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/common
 
+# Fonts
+PRODUCT_PACKAGES += \
+    LineageLatoFont \
+    LineageRubikFont
+
 PRODUCT_PACKAGES += \
     NavigationBarMode2ButtonOverlay
 
